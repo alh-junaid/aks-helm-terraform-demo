d5abea14bcd720737c868f9fbae02e383f84db8d
alh-junaid <alh.juned@gmail.com>
2025-08-16 20:17:12 +0530

    Updating tf backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "myResourceGroup"
+    resource_group_name  = "dev-tfstate-rg"
     storage_account_name = "gactfstatedemo1"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
